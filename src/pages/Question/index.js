fed229cc2d63bc8f359c28a4c39aef449e4d70e6
Arthur <bortoliniamm@gmail.com>
2021-06-03 16:09:36 +0200

    small fix

--- src/pages/Question/index.js
+++ src/pages/Question/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "@reach/router";
 import useData from "../../hooks/useData";
 import Answer from "./Answer";
-import Modal from "../../components/Modal";
 
 export default function Question() {
   const { id } = useParams();
@@ -17,7 +16,6 @@ export default function Question() {
       {!question?.answers.length ? (
         <div className="bg-green-900 text-gray-200 font-semibold text-xl p-4">
           No answers have been provided for this question.
-          <Modal />
         </div>
       ) : (
         <>
